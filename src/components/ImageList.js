7cd3e73e05ce89627c43e5628d43d70e93dff9b2
DLenet <dvsachs2@gmail.com>
2023-06-01 10:50:09 +0300

    firsts commit

---
+++ src/components/ImageList.js
@@ -0,0 +1,13 @@
+import ImageShow from "./ImageShow"
+import './imageList.css'
+
+function ImageList({ images }) {
+
+    const renderedImages = images.map((image) => <ImageShow key={image.id} image={image}/>)
+
+    return(
+        <div className="image-list">{renderedImages}</div>
+    )
+}
+
+export default ImageList
